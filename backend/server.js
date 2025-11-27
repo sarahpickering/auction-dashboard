2e78e5837270da4bee0f03096076ec6a2168831f
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-11-26 22:11:17 -0700

    initalize express erver and add survey endpoints

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const auctions = require("./data/auctions.json");
+const surveyResponses = require("./data/surveyResponses.json");
+
+// Get all auctions
+app.get("/api/auctions", (req, res) => {
+  res.json(auctions);
+});
+
+// Get all survey responses
+app.get("/api/surveys", (req, res) => {
+  res.json(surveyResponses);
+});
+
+// Submit a new survey response
+app.post("/api/surveys", (req, res) => {
+  const newResponse = req.body;
+
+  if (!newResponse || Object.keys(newResponse).length === 0) {
+    return res.status(400).json({ error: "Missing survey response data" });
+  }
+
+  surveyResponses.push(newResponse);
+
+  // Write updated data back to file
+  fs.writeFileSync(
+    "./data/surveyResponses.json",
+    JSON.stringify(surveyResponses, null, 2)
+  );
+
+  res.status(201).json({
+    message: "Survey response submitted",
+    response: newResponse,
+  });
+});
+
+
+const PORT = 5000;
+app.listen(POR
