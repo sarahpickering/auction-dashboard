b40b24827042998e742d47c233ef356e50d392a2
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-12-17 22:48:46 -0700

    Add basic request logging middleware

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,11 @@ app.get("/api/surveys", (req, res) => {
   res.json(surveyResponses);
 });
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Submit a new survey response
 app.post("/api/surveys", (req, res) => {
   const { auctionId, didBid, reason, overallExperience } = req.body;
